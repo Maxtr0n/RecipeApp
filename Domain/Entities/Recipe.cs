80cc4cf9475f60d5112bcb7a4825f5ad8ca3fb00
Schütz Máté <schutz.mate@gmail.com>
2023-08-28 13:53:16 +0200

    Initial work

---
+++ Domain/Entities/Recipe.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Recipe : EntityBase
+    {
+        public string Title { get; set; } = default!;
+
+        public IEnumerable<string> Ingredients { get; set; } = Enumerable.Empty<string>();
+
+        public string Description { get; set; } = default!;
+
+        public IEnumerable<string> Images { get; set; } = Enumerable.Empty<string>();
+
+        public string Author { get; set; } = default!;
+    }
+}
