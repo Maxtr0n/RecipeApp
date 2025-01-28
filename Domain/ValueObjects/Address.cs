f57b1140fd4ee29fe02165614a873ed2e1467afa
Schütz Máté <schutz.mate@gmail.com>
2025-01-28 11:53:19 +0100

    Add value object

---
+++ Domain/ValueObjects/Address.cs
@@ -0,0 +1,22 @@
+﻿using SharedKernel;
+
+namespace Domain.ValueObjects;
+
+public class Address(string street, string city, string state, string zipCode, string country)
+    : ValueObject
+{
+    public string Street { get; init; } = street;
+    public string City { get; init; } = city;
+    public string State { get; init; } = state;
+    public string ZipCode { get; init; } = zipCode;
+    public string Country { get; init; } = country;
+
+    protected override IEnumerable<object> GetEqualityComponents()
+    {
+        yield return Street;
+        yield return City;
+        yield return State;
+        yield return ZipCode;
+        yield return Country;
+    }
+}
