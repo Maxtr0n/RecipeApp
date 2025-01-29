7b60edc6172dcd6f51ef85cb543a383e5208b253
Schütz Máté <schutz.mate@gmail.com>
2025-01-29 15:29:57 +0100

    Integ test work

--- IntegrationTests/IntegrationTestWebAppFactory.cs
+++ IntegrationTests/IntegrationTestWebAppFactory.cs
@@ -4,11 +4,25 @@ using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.AspNetCore.TestHost;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using Testcontainers.MsSql;
 
 namespace IntegrationTests;
 
-public class IntegrationTestWebAppFactory : WebApplicationFactory<Program>
+public class IntegrationTestWebAppFactory : WebApplicationFactory<Program>, IAsyncLifetime
 {
+    private readonly MsSqlContainer _dbContainer =
+        new MsSqlBuilder().WithImage("mcr.microsoft.com/mssql/server:2022-latest").Build();
+
+    public Task InitializeAsync()
+    {
+        return _dbContainer.StartAsync();
+    }
+
+    public new Task DisposeAsync()
+    {
+        return _dbContainer.StopAsync();
+    }
+
     protected override void ConfigureWebHost(IWebHostBuilder builder)
     {
         builder.ConfigureTestServices(services =>
@@ -23,7 +37,7 @@ public class IntegrationTestWebAppFactory : WebApplicationFactory<Program>
 
             services.AddDbContext<RecipeDbContext>(options =>
             {
-                options.UseSqlServer("???");
+                options.UseSqlServer(_dbContainer.GetConnectionString());
             });
         });
     }
---
+++ IntegrationTests/Recipes/CreateRecipeTest.cs
@@ -0,0 +1,34 @@
+﻿using Application.Common.Dtos;
+using Application.Recipes.Create;
+using Domain.Entities;
+
+namespace IntegrationTests.Recipes;
+
+public class CreateRecipeTest : BaseIntegrationTest
+{
+    public CreateRecipeTest(IntegrationTestWebAppFactory factory) : base(factory)
+    {
+    }
+
+    [Fact]
+    public async Task Create_Should_Add_Recipe_To_Database()
+    {
+        // Arrange
+        var dto = new RecipeCreateDto
+        {
+            Title = "My Recipe",
+            Description = "My Recipe Description",
+            Ingredients = ["Ingredient 1, Ingredient 2, Ingredient 3"],
+            Images = []
+        };
+
+        var user = new ApplicationUser();
+        
+        var command = new CreateRecipeCommand(dto, user);
+
+        // Act
+
+
+        // Assert
+    }
+}
