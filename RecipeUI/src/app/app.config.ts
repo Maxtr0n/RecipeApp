77be215e17a38f7f5f274346979e727e14fe13d2
Schütz Máté <schutz.mate@gmail.com>
2025-10-05 20:37:54 +0200

    Add angular-auth-oidc-client package

--- RecipeUI/src/app/app.config.ts
+++ RecipeUI/src/app/app.config.ts
@@ -2,11 +2,14 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessC
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { authConfig } from './auth/auth.config';
+import { provideAuth } from 'angular-auth-oidc-client';
 
 export const appConfig: ApplicationConfig = {
-  providers: [
-    provideBrowserGlobalErrorListeners(),
-    provideZonelessChangeDetection(),
-    provideRouter(routes)
-  ]
+    providers: [
+        provideBrowserGlobalErrorListeners(),
+        provideZonelessChangeDetection(),
+        provideRouter(routes),
+        provideAuth(authConfig)
+    ]
 };
